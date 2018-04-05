472e8316747e709f58b07ac45a9e5593a6a844fe
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2018-04-05 15:05:28 +0200

    add main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,20 @@
+resource "azurerm_resource_group" "api_app" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+}
+
 resource "azurerm_template_deployment" "api_app" {
   name                = "${var.api_app_name}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${azurerm_resource_group.api_app.name}"
   template_body       = "${file("${path.module}/template.json")}"
 
   parameters {
     "name"               = "${var.api_app_name}"
     "serverFarmId"       = "${var.app_service_plan_id}"
     "apiDefinitionUrl"   = "${var.api_definition_url}"
-    "corsAllowedOrigins" = "${join(",", var.cors_allowed_origins)}"
+    "corsAllowedOrigins" = "${jsonencode(var.cors_allowed_origins)}"
     "location"           = "${var.location}"
+    "tags"               = "${jsonencode(var.tags)}"
   }
 
   deployment_mode = "Incremental"
